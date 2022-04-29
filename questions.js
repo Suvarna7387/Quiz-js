3e242ac7c556343fa6a729c4b1190682715f879a
Suvarna7387 <95468789+Suvarna7387@users.noreply.github.com>
2022-04-29 16:29:13 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "Hyper Text Markup Language",
+        option: [
+            "Hyper Tag Markup Language",
+            "Hyper Text Markup Language",
+            "Hyperlinks Text Mark Language",
+            "Hyperlinking Text Marking Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following attribute is used to provide a unique name to an element?",
+        answer: "id",
+        option: [
+            "class",
+            "id",
+            "type",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 3,
+        question: "CSS stands for -",
+        answer: "Cascading style sheets",
+        option: [
+            "Cascade style sheets",
+            "Color and style sheets",
+            "Cascading style sheets",
+            "None of the above",
+        ]
+
+    },
+    {
+        numb: 4,
+        question: "The CSS property used to control the element's font-size is -",
+        answer :"font-size",
+        option: [
+            "text-style",
+            "font-size",
+            "text-size",
+            "None of the above",
+        ]
+
+    },
+    {
+        numb: 5,
+        question: "Which type of JavaScript language is ___",
+        answer: "Object-Based",
+        option: [
+            "Object-Oriented",
+            "Object-Based",
+            "Assembly-language",
+            "High-level",
+        ]
+
+    }
+];
